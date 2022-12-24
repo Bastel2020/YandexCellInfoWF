2cfdf5c1789c34079c54623b1d043b14a1f4d54d
Bastel2020 <KirillandIra@gmail.com>
2022-12-24 15:17:45 +0500

    sectors storage

---
+++ YandexCellInfoWF/Models/SectorsStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexCellInfoWF.Models
+{
+    public class SectorsStorage
+    {
+        private Dictionary<int, int> _polularSectors { get; set; }
+        private IOrderedEnumerable<int> _otherSectors { get; set; }
+        private float _popularSectorRequestCount;
+        private
+
+        public SectorsStorage()
+        {
+            _otherSectors = new List<int>(Enumerable.Range(0, 256)).OrderBy(x => x);
+            _polularSectors = new Dictionary<int, int>();
+        }
+
+        public IEnumerable<int> GetAllSectors() => Enumerable.Range(0, 256);
+
+        public IEnumerable<int> GetNotPopularSectors() => _otherSectors;
+
+        public IEnumerable<int> GetPopularSectors()
+        {
+            _popularSectorRequestCount++;
+
+            var result = _polularSectors.Keys;
+
+            if (_popularSectorRequestCount % 10 == 0)
+                RemoveRareSectorsFromPopular();
+
+            return result;
+        }
+
+        private void RemoveRareSectorsFromPopular()
+        {
+
+        }
+    }
+}
